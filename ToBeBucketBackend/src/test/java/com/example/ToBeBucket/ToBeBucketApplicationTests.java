3f64de93ecda5c3ab1ae2fbf0cb62ad76ba86571
Jimin <gift040404@naver.com>
2024-11-09 14:47:01 +0900

    [init] 백엔드초기세팅
    mysql 서버 설정 필요

---
+++ ToBeBucketBackend/src/test/java/com/example/ToBeBucket/ToBeBucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ToBeBucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToBeBucketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
