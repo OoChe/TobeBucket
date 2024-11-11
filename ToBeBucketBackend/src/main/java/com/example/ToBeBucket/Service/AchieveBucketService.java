097b38953d2527185eb35df690d6698258b2a9ac
Jimin <gift040404@naver.com>
2024-11-12 01:39:14 +0900

    [FIX] 버킷달성기록 수정
    - 버킷번호 찾아서 기록할 수 있도록 수정(findBybucketId)
    - Bucket Entity에서 FK로 가져오는 bucketId를 MapsId로 표시

--- ToBeBucketBackend/src/main/java/com/example/ToBeBucket/Service/AchieveBucketService.java
+++ ToBeBucketBackend/src/main/java/com/example/ToBeBucket/Service/AchieveBucketService.java
@@ -1,8 +1,11 @@
 package com.example.ToBeBucket.Service;
 
 import com.example.ToBeBucket.DTO.AchieveBucketDTO;
+import com.example.ToBeBucket.DTO.SemiGoalDTO;
+import com.example.ToBeBucket.Entity.Bucket;
 import com.example.ToBeBucket.Entity.BucketAchievement;
 import com.example.ToBeBucket.Repository.AcheiveBucketRepository;
+import com.example.ToBeBucket.Repository.BucketRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,12 +15,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AchieveBucketService {
     private final AcheiveBucketRepository achieveBucketRepository;
+    private final BucketRepository bucketRepository;
 
     public void saveAchieveBucket(AchieveBucketDTO achieveBucketDTO) {
         // DTO에서 Entity로 변환
+        Bucket bucket = bucketRepository.findById(achieveBucketDTO.getBucketId())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid bucketId: " + achieveBucketDTO.getBucketId()));
+
         BucketAchievement achieveBucket = new BucketAchievement();
 
-        achieveBucket.setBucketId(achieveBucketDTO.getBucketId());
+        achieveBucket.setBucket(bucket);
         achieveBucket.setStickerId(achieveBucketDTO.getStickerId());
 
         // Optional 값 처리
@@ -34,4 +41,9 @@ public class AchieveBucketService {
         // Repository를 이용하여 DB에 저장
         achieveBucketRepository.save(achieveBucket);
     }
+
+    public void saveSemiGoalAhcivement(SemiGoalDTO semiGoalDTO){
+
+    }
+
 }
