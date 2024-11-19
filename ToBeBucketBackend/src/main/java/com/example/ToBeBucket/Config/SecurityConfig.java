be35b77d0f216a47d8a8de3ba68d1c801f6444a7
Jimin <gift040404@naver.com>
2024-11-19 19:10:18 +0900

    [feat] jwt 토큰 발급
    로그인 성공 시 토큰 발급

--- ToBeBucketBackend/src/main/java/com/example/ToBeBucket/Config/SecurityConfig.java
+++ ToBeBucketBackend/src/main/java/com/example/ToBeBucket/Config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.ToBeBucket.Config;
 
+import com.example.ToBeBucket.jwt.JWTUtil;
 import com.example.ToBeBucket.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,9 +18,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
     //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
     private final AuthenticationConfiguration authenticationConfiguration;
-
-    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+    //jwt util 변수
+    private final JWTUtil jwtUtil;
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil) {
         this.authenticationConfiguration = authenticationConfiguration;
+        this.jwtUtil=jwtUtil;
     }
 
     //AuthenticationManager Bean 등록
@@ -61,7 +64,7 @@ public class SecurityConfig {
         //필터 추가
             // LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
         http
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration),jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         //세션 설정
         http
--- ToBeBucketBackend/src/main/java/com/example/ToBeBucket/jwt/LoginFilter.java
+++ ToBeBucketBackend/src/main/java/com/example/ToBeBucket/jwt/LoginFilter.java
@@ -1,21 +1,29 @@
 package com.example.ToBeBucket.jwt;
 
+import com.example.ToBeBucket.DTO.CustomUserDetails;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.coyote.Response;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
-
-    public LoginFilter(AuthenticationManager authenticationManager) {
+    //jwt 토큰 발급을 위한
+    private final JWTUtil jwtUtil;
+    public LoginFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
 
         this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
     }
 
     @Override
@@ -36,12 +44,25 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     //로그인 성공시 실행하는 메소드 (여기서 JWT를 발급하면 됨)
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
-        System.out.println("successfulAuthentication ><!! ");
+        //유저 정보 가져오기
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        String username = customUserDetails.getUsername();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+
+        String role = auth.getAuthority();
+        //username과 role 값을 가지고 토큰 발급 요청하는 메서드임.
+        String token = jwtUtil.createJwt(username, role, 60*60*10L);
+
+        response.addHeader("Authorization", "Bearer " + token);
     }
 
     //로그인 실패시 실행하는 메소드
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
-        System.out.println("로그인 실패 ㅠㅠ!! ");
+        response.setStatus(401);
     }
 }
