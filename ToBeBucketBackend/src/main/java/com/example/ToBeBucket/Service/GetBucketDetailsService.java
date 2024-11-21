46dc40bc65800ca43ecdf7c7cf67f380263c8e99
Jimin <gift040404@naver.com>
2024-11-21 22:24:57 +0900

    [feat] 버킷 상세 조회 기능 - 함께 하는 친구 정보 추가

--- ToBeBucketBackend/src/main/java/com/example/ToBeBucket/Service/GetBucketDetailsService.java
+++ ToBeBucketBackend/src/main/java/com/example/ToBeBucket/Service/GetBucketDetailsService.java
@@ -18,26 +18,29 @@ public class GetBucketDetailsService {
     private final BucketSemiGoalRepository bucketSemiGoalRepository;
     private final AchieveBucketRepository achieveBucketRepository;
     private final UserLoginRepository userLoginRepository;
+    private final BucketFriendRepository bucketFriendRepository;
+    private final UserProfileRepository userProfileRepository;
 
     public Map<String, Object> getBucketDetail(String userId, Integer bucketId){
         Bucket bucket = bucketRepository.findByBucketId(bucketId);
         UserLogin user = userLoginRepository.findByUserId(userId);
         List<UserBucket> userBuckets = userBucketRepository.findByUserIdAndBucketId(user, bucket);
         if (userBuckets.isEmpty()) {
-            throw new RuntimeException("UserBucket not found for userId: " + userId + " and bucketId: " + bucketId);
+            throw new RuntimeException("userId: (" + userId + ", bucketId: " + bucketId + ") pair의 버킷이 없습니다.");
         }
-        UserBucket userBucket = userBuckets.get(0);
+        UserBucket userBucket = userBuckets.get(0); //이미 하나 밖에 없어서
         List<BucketSemiGoal> bucketSemiGoals = bucketSemiGoalRepository.findByBucket(userBucket.getBucketId());
         Optional<BucketAchievement> bucketAchievement = achieveBucketRepository.findByBucketId(bucketId);
+        List<BucketFriend> bucketFriendLists = bucketFriendRepository.findByBucket(bucket);
 
         Map<String, Object> bucketDetailMap = new LinkedHashMap<>();
-
         bucketDetailMap.put("bucketId", bucket.getBucketId());
         bucketDetailMap.put("bucketName", bucket.getBucketName());
         bucketDetailMap.put("bucketContent", bucket.getBucketContent());
         bucketDetailMap.put("createDate", bucket.getCreateDate());
         bucketDetailMap.put("goalDate", bucket.getGoalDate());
         bucketDetailMap.put("category", bucket.getCategory());
+
         //중간목표가 있는 버킷이라면 중간목표도 반환해야함
         if (!bucketSemiGoals.isEmpty()) {
             List<Map<String, Object>> semiGoalDataList = new ArrayList<>();
@@ -63,6 +66,18 @@ public class GetBucketDetailsService {
             if (bucketAchievement.get().getAchievementMedia() != null) {
                 bucketDetailMap.put("achievementMedia", bucketAchievement.get().getAchievementMedia());
             }
+        }
+
+        //함께하는 친구가 있는 경우 친구 정보도 반환해야 함.
+        if(!bucketFriendLists.isEmpty()){
+            List<String> friendNicknameLists = new ArrayList<>();
+            for (BucketFriend bucketFriend : bucketFriendLists) {
+                Optional<UserProfile> userProfileOpt = userProfileRepository.findByUserId(bucketFriend.getFriend().getUserId());
+                userProfileOpt.ifPresent(userProfile -> {
+                    friendNicknameLists.add(userProfile.getNickname());
+                });
+            }
+            bucketDetailMap.put("friendNickname", friendNicknameLists); // 리스트를 Map에 추가
         }
 
         return bucketDetailMap;
